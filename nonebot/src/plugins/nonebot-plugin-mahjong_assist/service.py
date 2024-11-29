d780ad6c2038951717e81839132957567c1ed740
AiNakow <AiNakowAlice@outlook.com>
2024-11-29 17:28:21 +0800

    调整图片分辨率

--- nonebot/src/plugins/nonebot-plugin-mahjong_assist/service.py
+++ nonebot/src/plugins/nonebot-plugin-mahjong_assist/service.py
@@ -1,5 +1,6 @@
 from nonebot_plugin_htmlrender import html_to_pic
 from PIL import Image
+import asyncio
 
 from .common import *
 from .template_env import *
@@ -7,10 +8,18 @@ from .template_env import *
 class MahjongService:
 
     @staticmethod
-    def tenhou_paili_analyse(type, tehai_input):
+    def tenhou_paili_analyse(analyse_type, tehai_input):
         t = jinja_env.get_template("tenhou_paili.html")
-        content = t.render(jsPath=os.path.join(template_dir, "1008.js"), typeStr=type, tehaiInputStr=tehai_input)
-        pic = html_to_pic(html=content, type="jpeg", quality=80, device_scale_factor=2)
+        content = t.render(jsPath=os.path.join(template_dir, "1008.js"), typeStr=analyse_type, tehaiInputStr=tehai_input)
+        
+        async def convert_html_to_pic(content): 
+            result = await html_to_pic(html=content, type="jpeg", quality=80, device_scale_factor=2) 
+            return result
+        
+        loop = asyncio.get_event_loop() 
+        pic = loop.run_until_complete(convert_html_to_pic(content=content))
+        print(type(pic))
+
         image = Image.frombytes(mode="RGB", size=(2560, 1440), data=pic)
         rect = ((2560 - 1200) / 2, 0, (2560 - 1200) / 2 + 1200, 1400)
         crop_image = image.crop(rect)
