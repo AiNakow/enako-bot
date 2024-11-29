8345f76853fa80c4a8ae28c1ff3aef4a23ddf7a7
AiNakow <AiNakowAlice@outlook.com>
2024-11-29 15:41:57 +0800

    调整图片分辨率

--- nonebot/src/plugins/nonebot-plugin-mahjong_assist/service.py
+++ nonebot/src/plugins/nonebot-plugin-mahjong_assist/service.py
@@ -9,7 +9,6 @@ class MahjongService:
     def tenhou_paili_analyse(type, tehai_input):
         t = jinja_env.get_template("tenhou_paili.html")
         content = t.render(jsPath=os.path.join(template_dir, "1008.js"), typeStr=type, tehaiInputStr=tehai_input)
-        print(content)
-        pic = html_to_pic(html=content, device_scale_factor=8)
+        pic = html_to_pic(html=content, device_scale_factor=0.5)
 
         return pic
