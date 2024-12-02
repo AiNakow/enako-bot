0e5ee647db25804cb6ee3ecf1dc8f5996da161f6
AiNakow <AiNakowAlice@outlook.com>
2024-12-02 10:37:30 +0800

    插件管理

--- nonebot/src/plugins/nonebot-plugin_management/__init__.py
+++ nonebot/src/plugins/nonebot-plugin_management/__init__.py
@@ -2,6 +2,7 @@ from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 from nonebot.permission import SUPERUSER
 from nonebot import on_command
+from nonebot import on_fullmatch
 from nonebot.rule import to_me
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
@@ -21,11 +22,21 @@ __plugin_meta__ = PluginMetadata(
 
 config = get_plugin_config(Config)
 
-plugin_list = on_command("日麻小助手", priority=10, block=True)
+check_enable = on_everyone_close = on_fullmatch("enako", priority=1, block=True)
+plugin_list = on_command("插件列表", priority=10, block=True)
+
+@check_enable.handle()
+async def check_enable_handler():
+    try:
+        await check_enable.finish("喵呜", at_sender=True)
+    except MatcherException:
+        raise
+    except Exception as e:
+        pass
 
 @plugin_list.handle()
-async def get_plgin_list_handle():
-    message = "可用插件帮助：\n"
+async def get_plgin_list_handler():
+    message = "可用的插件帮助：\n"
     for plugin in plugin_register.get_plugin_help_list():
         message += plugin + "\n"
         
