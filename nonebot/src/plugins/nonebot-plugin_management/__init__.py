ae38c16a6099913198e5495ad2d1e62d91e701f4
AiNakow <AiNakowAlice@outlook.com>
2024-12-19 10:03:55 +0800

    add echo 喵呜

--- nonebot/src/plugins/nonebot-plugin_management/__init__.py
+++ nonebot/src/plugins/nonebot-plugin_management/__init__.py
@@ -3,8 +3,10 @@ from nonebot.plugin import PluginMetadata
 from nonebot.permission import SUPERUSER
 from nonebot import on_command
 from nonebot import on_fullmatch
+from nonebot import on_message
 from nonebot.rule import to_me
 from nonebot.adapters import Message
+from nonebot.adapters import Event
 from nonebot.params import CommandArg
 from nonebot.params import Depends
 from nonebot.exception import MatcherException
@@ -21,8 +23,10 @@ __plugin_meta__ = PluginMetadata(
 )
 
 config = get_plugin_config(Config)
+neko_count = 0
 
 check_enable = on_fullmatch("enako", priority=1, block=True)
+echo_neko = on_message(priority=1, block=False)
 plugin_list = on_command("help", priority=10, block=True)
 
 @check_enable.handle()
@@ -33,6 +37,21 @@ async def check_enable_handler():
         raise
     except Exception as e:
         pass
+    
+@echo_neko.handle()
+async def echo_neko_handler(event: Event):
+    if event.get_plaintext() == "喵呜":
+        neko_count += 1
+    else:
+        neko_count = 0
+    if neko_count == 2:
+        neko_count = 0
+        try:
+            await echo_neko.finish("喵呜")
+        except MatcherException:
+            raise
+        except Exception as e:
+            pass
 
 @plugin_list.handle()
 async def get_plgin_list_handler():
