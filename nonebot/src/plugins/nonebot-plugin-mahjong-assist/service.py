fbff656b6800eb0ced0765942af6a5d97d59f258
AiNakow <ainakowalice@outlook.com>
2025-07-30 19:39:39 +0800

    为什么渲染超时呢

--- nonebot/src/plugins/nonebot-plugin-mahjong-assist/service.py
+++ nonebot/src/plugins/nonebot-plugin-mahjong-assist/service.py
@@ -1,4 +1,3 @@
-from nonebot_plugin_htmlrender import html_to_pic
 from PIL import Image
 import httpx
 import json
@@ -6,6 +5,8 @@ import base64
 import asyncio
 import nest_asyncio
 from io import BytesIO
+from nonebot_plugin_htmlrender import get_new_page, html_to_pic
+from nonebot.log import logger
 
 from .common import *
 from .template_env import *
@@ -13,8 +14,23 @@ from .template_env import *
 nest_asyncio.apply()
 
 async def convert_html_to_pic(content): 
-    result = await html_to_pic(html=content, type="jpeg", quality=80, device_scale_factor=2) 
+    result = await html_to_pic(html=content, type="jpeg", quality=70, device_scale_factor=2, wait=1000) 
     return result
+
+async def convert_html_to_pic2(content: str) -> BytesIO:
+    async with get_new_page(2.0) as page:
+        try:
+            page.on("console", lambda msg: logger.debug(f"浏览器控制台: {msg.text}"))
+            await page.goto(f"file://{os.getcwd()}")
+            await page.set_content(content, wait_until="networkidle")
+            await page.wait_for_timeout(1000)
+            pic = await page.screenshot(full_page=True, type="jpeg", quality=70, timeout=30000)
+            await page.close()
+            return pic
+        except Exception as e:
+            print(e)
+            raise e
+        
 class MahjongService:
 
     @staticmethod
@@ -22,7 +38,7 @@ class MahjongService:
         t = jinja_env.get_template("tenhou_paili.html")
         content = t.render(jsPath=os.path.join(template_dir, "1008.js"), typeStr=analyse_type, tehaiInputStr=tehai_input)
         
-        pic = asyncio.run(convert_html_to_pic(content=content))
+        pic = asyncio.run(convert_html_to_pic2(content=content))
 
         image = Image.open(BytesIO(pic))
         rect = ((2560 - 1400) / 2, 0, (2560 - 1400) / 2 + 1400, 1400)
