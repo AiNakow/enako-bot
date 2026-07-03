181768ffd8268cf58384dd24fa100f0e3a469115
AiNakow <ainakowalice@outlook.com>
2026-07-04 00:09:43 +0900

    修复jinja模板注入问题

--- nonebot/src/plugins/nonebot-plugin-gsz-assist/template_env.py
+++ nonebot/src/plugins/nonebot-plugin-gsz-assist/template_env.py
@@ -1,11 +1,30 @@
+import json
+
 from jinja2 import Environment, FileSystemLoader
+from markupsafe import Markup
 
 from .common import *
 
 
+def _tojson(obj):
+    """Serialize to JSON, escaping characters unsafe in <script> context."""
+    s = json.dumps(obj, ensure_ascii=False, separators=(",", ":"))
+    s = (
+        s.replace("<", "\\u003c")
+        .replace(">", "\\u003e")
+        .replace("&", "\\u0026")
+        .replace("'", "\\u0027")
+    )
+    return Markup(s)
+
+
 def create_jinja2_env():
     """创建 Jinja2 环境"""
-    env = Environment(loader=FileSystemLoader(template_dir))
+    env = Environment(
+        loader=FileSystemLoader(template_dir),
+        autoescape=True,
+    )
+    env.filters["tojson"] = _tojson
     return env
 
 
